3d5064b963ba269156af36d4e3f18975e5ed9e18
S.Hale <sally.hale@homeport.org.uk>
2020-10-09 15:52:26 +0100

    correct spelling errors (#11)
    Thank you !

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var categories = require('../json/categories.json');
-var exercice = require('../json/questions.json');
+var exercise = require('../json/questions.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Exercice', questions: exercice.questions });
+	res.render('index', { title: 'Exercise', questions: exercise.questions });
 });
 
 module.exports = router;
